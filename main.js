85637c50f1c428c716a290e0c71a506473f6335b
Rutendo <rutendosvisveni@gmail.com>
2021-04-21 23:27:20 +0200

    Working

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var numArray = [140000, 104, 99];
+var numArray = [24,8,23,32,5,62];
 numArray.sort(function(a, b) {
   return a - b;
 });
+
+console.log(numArray);
+document.getElementById("out").innerHTML = numArray;
